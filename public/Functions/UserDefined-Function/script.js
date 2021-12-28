5897e65d011a8e3d8423445809bc3192d60f90c0
nidhiprajapati0820 <nidhi.khurpia@gmail.com>
2021-12-27 19:00:33 -0700

    Initial commit

---
+++ public/Functions/UserDefined-Function/script.js
@@ -0,0 +1,21 @@
+var vijaykumar=function()
+{
+var fname="kumar",fph=6785678578,femail="kumar@gmail.com";
+document.write("Faculty Name:"+fname+"<br />");
+document.write("Faculty phno:"+fph+"<br />");
+document.write("faculaty Email:"+femail+"<br />");
+return "Hello"
+
+
+}
+document.getElementById("demo").innerHTML= vijaykumar();
+
+
+/*
+var variable=value;
+var variable=Array(value,value,value,value,..)
+var variable=functionname();
+var variable=function(){
+----
+};
+*/
---
+++ public/Functions/UserDefined-Function/tablescript.js
@@ -0,0 +1,9 @@
+function multiplicationtable()
+{
+var n=5;
+for(var j=1;j<=10;j++)
+{
+document.write(n+"x"+j+"="+n*j+"<br />");
+}
+
+}
